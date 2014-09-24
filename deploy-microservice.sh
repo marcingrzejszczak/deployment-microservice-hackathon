3351ab1f5d4c2136c0f0332ff4caefd0e55b6b3e
Lukasz Szczesny <luk@mimuw.edu.pl>
2014-09-24 14:06:25 +0200

    Sample deployment script

---
+++ deploy-microservice.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+[[ -z $DEBUG ]] || set -o xtrace
+
+set -o errexit
+
+log() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@"
+}
+
+err() {
+    log "$@" >&2
+}
+
+usage() {
+    echo "usage: $( basename $0 ) groupId artifactId version"
+    exit 1
+}
+
+NEXUS_URL="${RD_OPTION_NEXUSURL:-$NEXUS_URL}"
+GROUP_ID="${RD_OPTION_GROUPID:-$1}"
+ARTIFACT_ID="${RD_OPTION_ARTIFACTID:-$2}"
+VERSION="${RD_OPTION_VERSION:-$3}"
+
+if [[ -z "${GROUP_ID}" ]]; then
+    err "Missing groupID"
+    usage
+fi
+
+if [[ -z "${ARTIFACT_ID}" ]]; then
+    err "Missing artifactId"
+    usage
+fi
+
+if [[ -z "${VERSION}" ]]; then
+    err "Missing version"
+    usage
+fi
+
+ARTIFACT_URL="${NEXUS_URL}/${GROUP_ID/./\/}/${ARTIFACT_ID}/${VERSION}/${ARTIFACT_ID}-${VERSION}.jar"
+
+echo $ARTIFACT_URL
+
